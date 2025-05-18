63c18f353986e1e92ff5f55bfe236ada4a544cad
OlivierdeSwart <94440732+OlivierdeSwart@users.noreply.github.com>
2025-05-18 15:09:20 -0300

    improved label names

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from apps import app001_subway_fees, app002_subway_tax_id, app003_correspondence
 st.sidebar.title("🧭 Navigation")
 app_choice = st.sidebar.radio("Select App", [
     "💡 Choose an App",
-    "001 Subway Fees",
-    "002 Subway Tax Id",
-    "003 Correspondence Lookup",
-    "004 Interchange Summary"
+    "001 Search By Entity ID",
+    "002 Search By Tax ID",
+    "003 Correspondence by Tax ID",
+    "004 Subway Research Request Responses"
 ])
 
 # ⛔ Detect app switch and clear cache
@@ -19,13 +19,13 @@ elif st.session_state.last_app_choice != app_choice:
     st.session_state.last_app_choice = app_choice
 
 # ✅ Route to selected app
-if app_choice == "001 Subway Fees":
+if app_choice == "001 Search By Entity ID":
     app001_subway_fees.run()
-elif app_choice == "002 Subway Tax Id":
+elif app_choice == "002 Search By Tax ID":
     app002_subway_tax_id.run()
-elif app_choice == "003 Correspondence Lookup":
+elif app_choice == "003 Correspondence by Tax ID":
     app003_correspondence.run()
-elif app_choice == "004 Interchange Summary":
+elif app_choice == "004 Subway Research Request Responses":
     app004_interchange_summary.run()
 else:
     st.title("👋 Welcome")
